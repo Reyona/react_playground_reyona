bdb1cc8612a2d392d72bd84eb745f71053131e9f
reyona <nokoru_ryohei_00@163.com>
2022-11-20 20:17:57 +0800

    add: routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-
+import { Route, Link } from 'react-router-dom';
 import { Menu, ConfigProvider, Layout } from 'antd';
 import zhCN from 'antd/lib/locale/zh_CN';
 import Demo from '@pages/demo';
@@ -9,23 +9,31 @@ import Settings from '@pages/settings'
 
 const { Header, Content, Footer } = Layout;
 
-function App() {
+function App({ match }: any) {
+  const defaultKey = match.url.replace('/', '') || 'demo';
   const projectUrl = 'https://github.com/Reyona/react_playground_reyona'
   return (
     <ConfigProvider locale={zhCN}>
       <Layout className="app">
         <Header>
-          <Menu mode="horizontal" theme="dark" defaultSelectedKeys={["demo"]} className="menu">
-            <Menu.Item key="demo">Demo</Menu.Item>
-            <Menu.Item key="employee">员工管理</Menu.Item>
-            <Menu.Item key="setting">系统设置</Menu.Item>
+          <Menu mode="horizontal" theme="dark" defaultSelectedKeys={ [ defaultKey ] } className="menu">
+            <Menu.Item key="demo">
+              <Link to="/demo">Demo</Link>
+            </Menu.Item>
+            <Menu.Item key="employee">
+              <Link to="/employee">员工管理</Link>
+            </Menu.Item>
+            <Menu.Item key="settings">
+              <Link to="/settings">系统设置</Link>
+            </Menu.Item>
           </Menu>
         </Header>
         <Content className="contentWrap">
           <div className="content">
-            <Demo />
-            <Employee />
-            <Settings />
+            <Route path="/" exact component={ Demo } />
+            <Route path="/demo" component={ Demo } />
+            <Route path="/employee" component={ Employee } />
+            <Route path="/settings" component={ Settings } />
           </div>
         </Content>
         <Footer className="footer">
